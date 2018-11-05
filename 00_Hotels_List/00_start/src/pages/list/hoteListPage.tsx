f9232ee390be602f9bb51065f6cd4f0730ebc8b7
Braulio <braulio.diez@gmail.com>
2018-11-05 18:33:27 +0100

    start completed

--- 00_Hotels_List/00_start/src/pages/list/hoteListPage.tsx
+++ 00_Hotels_List/00_start/src/pages/list/hoteListPage.tsx
@@ -7,13 +7,31 @@ import { withRouter, RouteComponentProps } from 'react-router-dom';
 import Typography from "@material-ui/core/Typography/Typography";
 import { HotelEntity } from "../../model";
 import { getHotelList } from "../../api/hotel";
+import CardMedia from "@material-ui/core/CardMedia/CardMedia";
+import CardActions from "@material-ui/core/CardActions/CardActions";
+import IconButton from "@material-ui/core/IconButton/IconButton";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import Avatar from "@material-ui/core/Avatar/Avatar";
+import red from '@material-ui/core/colors/red';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 // https://material-ui.com/guides/typescript/
 const styles = theme => createStyles({
   card: {
     width: '500px',
-    marginTop: '10px',    
+    marginTop: '10px',
   },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },  
 });
 
 interface State {
@@ -44,27 +62,54 @@ class HotelListPageInner extends React.Component<Props, State> {
     const { classes } = this.props;
     return (
       <div style={
-          {
-            display:'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between'
-          }}>
+        {
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between'
+        }}>
         {
           this.state.hotelList.map(
-            (hotel) =>            
+            (hotel) =>
               <Card className={classes.card} key={hotel.id}>
-                <CardHeader title={hotel.name} />
+                <CardHeader 
+                  avatar={
+                    <Avatar aria-label="Recipe" className={classes.avatar}>
+                    {hotel.hotelRating}
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton>
+                      <MoreVertIcon />
+                    </IconButton>
+                  }                  
+                  title={hotel.name} 
+                  subheader={hotel.address1}
+                />
                 <CardContent>
                   <div style=
-                    {{ display: 'flex', 
-                       flexDirection: 'column', 
-                       justifyContent: 'center' }}>
-                    <img src={hotel.thumbNailUrl} style={{width:'auto', maxHeight: '200px', margin:'auto'}}></img>
+                    {{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center'
+                    }}>
+                    <CardMedia
+                      className={classes.media}
+                      image={hotel.thumbNailUrl}
+                      title={hotel.name}
+                    />
                     <Typography variant="subtitle1" gutterBottom>
                       {hotel.shortDescription}
-                  </Typography>
+                    </Typography>
                   </div>
                 </CardContent>
+                <CardActions className={classes.actions} disableActionSpacing>
+                  <IconButton aria-label="Add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="Share">
+                    <ShareIcon />
+                  </IconButton>
+                </CardActions>
               </Card>
           )
         }
